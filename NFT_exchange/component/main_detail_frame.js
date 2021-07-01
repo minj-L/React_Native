ba854455608200a5825eb3e818b4bbefdaf3a013
minj-L <wonder_minji@naver.com>
2021-07-01 17:40:09 +0900

    main_detail_frame

---
+++ NFT_exchange/component/main_detail_frame.js
@@ -0,0 +1,107 @@
+//메인 상세정보 frame component
+/*import React from 'react';
+import {StyleSheet, View, Text, Button, TouchableOpacity, TouchableOpacityBase} from 'react-native';
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.elem}>
+        <View style={styles.userInfo}>
+          <View style={styles.profile}/>
+          <Text style={styles.name}>hyunji</Text>
+        </View>
+      </View>
+      <Text style={styles.nft_name}>NATURE</Text>
+      <View style={styles.elem}>
+        <View style={styles.colum}>
+          <Text style={styles.text}>현재 경매가</Text>
+          <Text style={styles.bold_text}>0.1 ETH</Text>
+        </View>
+        <View style={styles.colum}>
+          <Text style={styles.text}>남은 시간</Text>
+          <Text style={styles.bold_text}>00:00:00</Text>
+        </View>
+      </View>
+      <View style={styles.elem}>
+          <Text style={styles.text}>10.000원</Text>
+      </View>
+      <View style={styles.elem}>
+        <TouchableOpacity style={styles.button}>
+            <Text style={styles.title}>제안하기</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({ //원하는 구성 요소들은 여기서 설정해줘야 한다.
+  container: {
+    flex:1,
+  },
+  elem: { //프로필, 이름등을 가지고 있는 한 줄
+    width: '40%',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    marginTop: 10,
+  },
+
+  colum: {
+    width: '100%',
+    flexDirection: 'column',
+    marginTop: 10,
+  },
+
+  profile: { //검정색 원의 프로필
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+    backgroundColor: 'black',
+    marginLeft: 20,
+  },
+
+  userInfo: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+
+  name: {
+    fontSize: 20,
+    paddingLeft: 20,
+  },
+
+  nft_name: {
+    fontSize: 40,
+    fontWeight: 'bold',
+    marginLeft: 20,
+    marginTop: 10,
+  },
+
+  text: {
+    fontSize: 15,
+    marginLeft: 20,
+  },
+
+  bold_text: {
+    fontSize: 35,
+    fontWeight: 'bold',
+    marginLeft: 20,
+  },
+  button: {
+    marginLeft: 20,
+    borderRadius: 40,
+    width: 140,
+    height: 50,
+    backgroundColor:'#000000',
+  },
+
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: 'white',
+    marginTop: 10,
+    marginLeft: 30
+  }
+});
+
+export default App;*/

2fe6fa617723bedbf8aca3810884712d071800f5
minj-L <wonder_minji@naver.com>
2021-07-02 17:40:08 +0900

    Navigator_tutorial

--- NFT_exchange/component/Navigator_tutorial.js
+++ NFT_exchange/component/Navigator_tutorial.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+/*import React, { Component } from 'react'
 import {
   Text,
   View,
@@ -45,4 +45,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default App;*/

9f9c335e402055f6a8d3f21326e1a5b71887e287
minj-L <wonder_minji@naver.com>
2021-08-05 11:32:35 +0900

    파일정리

--- ETHWallet/src/App.js
+++
@@ -1,8 +0,0 @@
-import { createStackNavigator, createAppContainer } from 'react-native'
-import WalletsScreen from './components/WalletsScreen';
-
-const AppStackNavigator = createStackNavigator({
-    Wallets: { screen: WalletsScreen },
-});
-
-export default createAppContainer(AppStackNavigator);
